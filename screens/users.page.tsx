24229333579b285c7254d07e815961b67c0e342e
Duc Anh <nducanh1010@gmail.com>
2023-09-20 09:42:02 +0700

    quickstart react

---
+++ screens/users.page.tsx
@@ -0,0 +1,11 @@
+import UsersTable from "../components/users/users.table";
+
+const UsersPage = () => {
+  return (
+    <div>
+      page user
+      <UsersTable />
+    </div>
+  );
+};
+export default UsersPage;
