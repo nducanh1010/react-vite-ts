8baf140e67a686a0bbe575239a178509b07d41da
DESKTOP-JU1F7QJ\admin <nducanh1010@gmail.com>
2023-10-09 16:47:02 +0700

    add new user form

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "../../styles/users.css";
-import { Button, Table } from "antd";
+import { Button, Input, Modal, Table } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
 import { ColumnsType } from "antd/es/table";
 interface IUser {
   _id: string;
@@ -10,6 +12,13 @@ interface IUser {
 }
 const UsersTable = () => {
   const [listUsers, setListUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [role, setRole] = useState("");
+  const [address, setAddress] = useState("");
   useEffect(() => {
     console.log("useEffect");
     fetchData();
@@ -52,26 +61,95 @@ const UsersTable = () => {
     { title: "Name", dataIndex: "name" },
     { title: "Role", dataIndex: "role" },
   ];
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <Table columns={columnTable} dataSource={listUsers} rowKey={"_id"} />
-      {/* <table>
-        <thead>
-          <tr>
-            <th>Company</th>
-            <th>Contact</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-          </tr>
-        </tbody>
-      </table> */}
-    </div>
+    <>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button type="primary" onClick={showModal} icon={<PlusOutlined />}>
+          Add new User
+        </Button>
+        <Modal
+          maskClosable={false}
+          title="Add new user"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <div>
+            <label> Name:</label>
+            <Input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+          <div>
+            <label> Email:</label>
+
+            <Input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </div>
+          <div>
+            <label> Password:</label>
+
+            <Input
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </div>
+          <div>
+            <label> Address:</label>
+
+            <Input
+              value={address}
+              onChange={(event) => setAddress(event.target.value)}
+            />
+          </div>
+          <div>
+            <label> Gender :</label>
+
+            <Input
+              value={gender}
+              onChange={(event) => setGender(event.target.value)}
+            />
+          </div>
+          <div>
+            <label> Age:</label>
+
+            <Input
+              value={age}
+              onChange={(event) => setAge(event.target.value)}
+            />
+          </div>
+          <div>
+            <label> Role:</label>
+
+            <Input
+              value={role}
+              onChange={(event) => setRole(event.target.value)}
+            />
+          </div>
+        </Modal>
+      </div>
+      <div>
+        <Table columns={columnTable} dataSource={listUsers} rowKey={"_id"} />
+      </div>
+    </>
   );
 };
 export default UsersTable;
