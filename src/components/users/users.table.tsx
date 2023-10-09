e887f359c940ebaab2c2980ecb4e5adc43b7c617
DESKTOP-JU1F7QJ\admin <nducanh1010@gmail.com>
2023-10-09 16:12:09 +0700

    quickstart show user antd

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,44 +1,76 @@
+import { useEffect, useState } from "react";
 import "../../styles/users.css";
+import { Button, Table } from "antd";
+import { ColumnsType } from "antd/es/table";
+interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+}
 const UsersTable = () => {
+  const [listUsers, setListUsers] = useState([]);
+  useEffect(() => {
+    console.log("useEffect");
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const res = await fetch("http://localhost:8000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+
+      body: JSON.stringify({
+        username: "admin@gmail.com",
+        password: "123456",
+      }),
+    });
+    const data = await res.json();
+    const accessToken = await data.data.access_token;
+    const resp = await fetch("http://localhost:8000/api/v1/users/all", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const usersData = await resp.json();
+    setListUsers(usersData.data.result);
+    console.log("check data", data);
+    console.log("check data users", usersData);
+    // setListUsers(usersData)
+  };
+  const columnTable: ColumnsType<IUser> = [
+    {
+      title: "Email",
+      dataIndex: "email",
+      render: (value, record) => {
+        return <a>{record.email}</a>;
+      },
+    },
+    { title: "Name", dataIndex: "name" },
+    { title: "Role", dataIndex: "role" },
+  ];
   return (
     <div>
-      <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
-      </table>
+      <Table columns={columnTable} dataSource={listUsers} rowKey={"_id"} />
+      {/* <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+        </tbody>
+      </table> */}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { FireOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import UsersPage from "./screens/users.page.tsx";
+import { MenuProps, Menu } from "antd";
+const items: MenuProps["items"] = [
+  {
+    label: <Link to={"/"}>Home</Link>,
+    key: "home",
+    icon: <FireOutlined />,
+  },
+  {
+    label: <Link to={"/users"}>Manage Users</Link>,
+    key: "user",
+    icon: <UserOutlined />,
+  },
+];
+
+const Header = () => {
+  const [current, setCurrent] = useState("home");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/users",
-    element: <UsersPage />,
+    element: <LayoutAdmin />,
+    children: [
+      { index: true, element: <App /> },
+      {
+        path: "users",
+        element: <UsersPage />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")!).render(
